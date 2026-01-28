78d5570da528d8baaa92c06c73f9905cfc862283
youssef <ysfa7md@gmail.com>
2026-01-28 15:24:39 +0200

    Refactor LinkedList class to properly initialize tail and size attributes, and correct append method implementation

--- linked_list.py
+++ linked_list.py
@@ -6,10 +6,21 @@ class Node:
 
 class LinkedList():
     def __init__(self, val):
-
         self.head=Node(val)
+        self.tail=Node(val)
+        self.size=1
 
-    def append(slef,val):
-        Node(val)
+    def append(self,val):
+        self.tail.next=Node(val)
+        self.size+=1
+
+    def delete(self,idx):
+        ptr=self.head
+
+        for _ in range(idx):
+            crnt=ptr.next
+        
+
+
 
 
