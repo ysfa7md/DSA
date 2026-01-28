3e24dbf12a4a4667dc4d23767e0a5d11082282aa
youssef <ysfa7md@gmail.com>
2026-01-28 15:13:48 +0200

    init commit

---
+++ linked_list.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+
+
+class LinkedList():
+    def __init__(self, val):
+
+        self.head=Node(val)
+
+    def append(slef,val):
+        Node(val)
+
+
