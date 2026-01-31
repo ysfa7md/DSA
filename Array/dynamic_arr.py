ae70938502b3fdcd7a1d680541dfe91e0a00c79e
youssef <ysfa7md@gmail.com>
2026-01-31 19:23:12 +0200

    Refactor DynamicArray class: implement missing methods, improve array management, and enhance functionality

--- Array/dynamic_arr.py
+++ Array/dynamic_arr.py
@@ -1,48 +1,99 @@
 class DynamicArray:
     def __init__(self, cap=2):
-        self.cap=cap
-        self.size=0
-        self.arr=[]
+        self.cap = cap
+        self.size = 0
+        self.arr = [None] * cap
+
+    def _resize(self, new_cap):
+        new_arr = [None] * new_cap
+        for i in range(new_cap):
+            new_arr[i] = self.arr[i]
 
     def size(self):
-        ...
+        return self.size
 
     def capacity(self):
-        ...
+        return self.cap
 
     def is_empty(self):
-        ...
+        return bool(self.size)
 
-    def get(self,index):
-        ...
+    def get(self, index):
+        if index >= self.size or index < 0:
+            raise Exception("out of range!")
+        return self.arr[index]
 
-    def set(self,index, value):
-        ...
+    def set(self, index, value):
+        if index > self.size or index < 0:
+            raise Exception("out of range!")
+        self.arr[index] = value
 
-    def append(self,value):
-        ...
+    def append(self, value):
+        _size = self.size
+        if _size == self.cap:
+            self._resize(_size * 2)
+        self.arr[self.size] = value
+        self.size += 1
 
     def pop(self):
-        ...
+        self.size -= 1
+        return self.arr[self.size]
 
-    def insert(self,index, value):
-        ...
+    def insert(self, index, value):
+        _size = self.size
+        if _size == self.cap:
+            self._resize(_size*2)
 
-    def remove(self,index):
-        ...
+        for i in range(self.size, index, -1):
+            self.arr[i] = self.arr[i + 1]
+        self.arr[index]=value
+        self.size += 1
+
+    def remove(self, index):
+        for i in range(index, self.size):
+            self.arr[i] = self.arr[i + 1]
+        self.size -= 1
 
     def clear(self):
-        ...
+        self.size = 0
 
-    def destroy(self):
-        ...
+    # def destroy(self):
+    #     ...
 
-    def find(self,value):
-        ...
+    def find(self, value):
+        for i in range(self.size):
+            if self.arr[i] == value:
+                return i
+            return -1
 
-    def contains(self,value):
-        ...
+    def __contains__(self, value):
+        for i in range(self.size):
+            if self.arr[i] == value:
+                return True
+            return False
 
-    def resize(self,new_capacity):
-        ...
+    def __iter__(self):
+        for i in range(self.size):
+            yield self.arr[i]
 
+    def __repr__(self):
+        r='Array(['
+        for i in range(self.size):
+            r=str(self.arr[i])+', '+r
+        r=r+'])'
+
+        return r
+
+    def __str__(self):
+        r = "["
+        for i in range(self.size):
+            r = str(self.arr[i]) + "," + r
+        r = r + "])"
+
+        return r
+
+def main():
+    ...
+
+if __name__== '__main__':
+    main()
