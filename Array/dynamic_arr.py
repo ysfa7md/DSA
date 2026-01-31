25e6cdb32832a598fa4b5d193a06246be8c31f14
youssef <ysfa7md@gmail.com>
2026-01-31 20:04:28 +0200

    Refactor DynamicArray class: fix resizing logic, improve boundary checks, and enhance string representation

--- Array/dynamic_arr.py
+++ Array/dynamic_arr.py
@@ -6,8 +6,10 @@ class DynamicArray:
 
     def _resize(self, new_cap):
         new_arr = [None] * new_cap
-        for i in range(new_cap):
+        for i in range(self.cap):
             new_arr[i] = self.arr[i]
+        self.arr = new_arr
+        self.cap = new_cap
 
     def size(self):
         return self.size
@@ -16,7 +18,7 @@ class DynamicArray:
         return self.cap
 
     def is_empty(self):
-        return bool(self.size)
+        return self.size == 0
 
     def get(self, index):
         if index >= self.size or index < 0:
@@ -24,7 +26,7 @@ class DynamicArray:
         return self.arr[index]
 
     def set(self, index, value):
-        if index > self.size or index < 0:
+        if index >= self.size or index < 0:
             raise Exception("out of range!")
         self.arr[index] = value
 
@@ -44,8 +46,9 @@ class DynamicArray:
         if _size == self.cap:
             self._resize(_size*2)
 
-        for i in range(self.size, index, -1):
-            self.arr[i] = self.arr[i + 1]
+        for i in range(self.size, index-1, -1):
+            self.arr[i+1] = self.arr[i]
+
         self.arr[index]=value
         self.size += 1
 
@@ -57,14 +60,16 @@ class DynamicArray:
     def clear(self):
         self.size = 0
 
-    # def destroy(self):
-    #     ...
+    def destroy(self):
+        self.arr = []
+        self.size = 0
+        self.cap = 0
 
     def find(self, value):
         for i in range(self.size):
             if self.arr[i] == value:
                 return i
-            return -1
+        return -1
 
     def __contains__(self, value):
         for i in range(self.size):
@@ -76,24 +81,57 @@ class DynamicArray:
         for i in range(self.size):
             yield self.arr[i]
 
+    def __len__(self):
+        return self.size
+
     def __repr__(self):
         r='Array(['
         for i in range(self.size):
-            r=str(self.arr[i])+', '+r
-        r=r+'])'
-
+            r+=str(self.arr[i])+','
+        r=r.rstrip(',')+'])'
         return r
 
     def __str__(self):
-        r = "["
+        r='['
         for i in range(self.size):
-            r = str(self.arr[i]) + "," + r
-        r = r + "])"
-
+            r+=str(self.arr[i])+','
+        r=r.rstrip(',')+']'
         return r
 
 def main():
-    ...
+    da=DynamicArray()
+    da.append(10)
+    da.append(20)
+    da.append(30)
+    da.append(40)
+    da.append(50)
+    print('initial array:', da)
+    print('length:',len(da))
+    print('insert 9 at index 2:',end=' ')
+    da.insert(2,9)
+    print(da)
+    print('get index 2:',end=' ')
+    print(da.get(2))
+    print('remove index 3:')
+    da.remove(3)
+    print('insert at index 0:',end=' ')
+    da.set(0,100)
+    print(da)
+    print("delete last:", da.pop())
+    print("length after pop:", len(da))
+    print(da)
+    print("find 10:", da.find(10))
+    print("find 40:", da.find(40))
+    print('is empty?:',da.is_empty())
+    da.clear()
+    print('after clear:')
+    print('is empty?:',da.is_empty())
+    # print(100 in da)
+    # print(20 in da)
+    # print('clean:',end=' ')
+    # da.clear()
+    # print(len(da))
+    # print(da)
 
 if __name__== '__main__':
     main()
